c54935e21f53fc257bbca70659b8b5baf6f90817
Suriya Kandaswamy <suriya.kandaswamy@gmail.com>
2021-01-26 18:34:16 -0500

    add scripts for windows and linux

--- run-packager.sh
+++ run-packager.sh
@@ -12,20 +12,32 @@ echo Building frontend...
 
 # compile python into executable
 echo Building executable...
-python build-macos.py py2app
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    python build-macos.py py2app
+elif [[ "$OSTYPE" == "msys" ]]; then # use MinGW
+    pyinstaller build-windows.spec
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    pyinstaller build-linux.spec --onefile
+fi
 
-# MAC OS DMG APP DISTRIBUTOR
-echo Building DMG...
-create-dmg --volname "Bean Counter Installer" \
-           --volicon assets/logo.icns \
-           --background assets/dmg_background.png \
-           --window-pos 200 120 \
-           --window-size 600 400 \
-           --icon-size 100 \
-           --icon "Bean Counter.app" 100 200 \
-           --hide-extension "Bean Counter.app" \
-           --app-drop-link 500 200 \
-           "Bean Counter Installer.dmg" dist
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # MAC OS DMG APP DISTRIBUTOR
+    echo Building DMG...
+    create-dmg --volname "Bean Counter Installer" \
+               --volicon assets/logo.icns \
+               --background assets/dmg_background.png \
+               --window-pos 200 120 \
+               --window-size 600 400 \
+               --icon-size 100 \
+               --icon "Bean Counter.app" 100 200 \
+               --hide-extension "Bean Counter.app" \
+               --app-drop-link 500 200 \
+               "Bean Counter Installer.dmg" dist
+elif [[ "$OSTYPE" == "msys" ]]; then # use MinGW
+    echo Windows packager not implemented yet
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo Linux packager not implemented yet
+fi
 
 # final cleanup
 echo Cleaning extras...
